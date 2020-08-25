c1341a561cd002b86fc090ce6b21c0d45364650a
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-08-25 10:08:07 +0530

    next build

---
+++ build/precache-manifest.7af6b503265ac19be6445f1a7851473a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec83ca8205a5d796107d83bd053a8d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "938be46e98a8de56654d",
+    "url": "/static/css/main.6bb45fd2.chunk.css"
+  },
+  {
+    "revision": "b28df03c76f30327af69",
+    "url": "/static/js/2.ae0b69aa.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/static/js/2.ae0b69aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "938be46e98a8de56654d",
+    "url": "/static/js/main.2fb645ba.chunk.js"
+  },
+  {
+    "revision": "33a5a858be361e26b205",
+    "url": "/static/js/runtime-main.687a1e43.js"
+  }
+]);
